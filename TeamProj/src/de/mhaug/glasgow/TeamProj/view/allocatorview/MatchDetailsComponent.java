b43fc5ada9e3cd91d2c493b012b10f7233f64e03
Martin Haug <martinhaug@smart-mail.de>
2015-09-22 23:56:44 +0200

    + Code der beim Refactoring übersehen wurde reinkopiert

--- TeamProj/src/de/mhaug/glasgow/TeamProj/view/allocatorview/MatchDetailsComponent.java
+++ TeamProj/src/de/mhaug/glasgow/TeamProj/view/allocatorview/MatchDetailsComponent.java
@@ -1,7 +1,22 @@
 package de.mhaug.glasgow.TeamProj.view.allocatorview;
 
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 class MatchDetailsComponent extends JPanel {
+	public MatchDetailsComponent() {
+		this.setLayout(new GridLayout(4, 2));
 
+		this.add(new JLabel("Week the match is held in:"));
+		this.add(new JTextField(15));
+
+		this.add(new JLabel("Area the match is held in:"));
+		this.add(new JTextField(15));
+
+		this.add(new JLabel("Level of the match:"));
+		this.add(new JTextField(15));
+	}
 }
--- TeamProj/src/de/mhaug/glasgow/TeamProj/view/allocatorview/SuitableRefereesComponent.java
+++ TeamProj/src/de/mhaug/glasgow/TeamProj/view/allocatorview/SuitableRefereesComponent.java
@@ -1,7 +1,27 @@
 package de.mhaug.glasgow.TeamProj.view.allocatorview;
 
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JScrollPane;
 
-class SuitableRefereesComponent extends JScrollPane {
+import de.mhaug.glasgow.TeamProj.model.Referee;
+import de.mhaug.glasgow.TeamProj.model.RefereeList;
 
+class SuitableRefereesComponent extends JScrollPane {
+	public SuitableRefereesComponent() {
+		super(new JList<>(new RefereeListModel()));
+	}
+}
+
+class RefereeListModel extends DefaultListModel<Referee> {
+	public RefereeListModel() {
+		for (Referee ref : RefereeList.getReadOnlyList())
+			if (isSuitable(ref))
+				this.addElement(ref);
+	}
+
+	private boolean isSuitable(Referee ref) {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
